342597d924497f4d11697c7116f7d1e5ceb7f02d
hwlee-ds <funtaland@gmail.com>
2024-08-07 06:40:12 +0000

    preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -16,14 +16,17 @@ CAT_FEATURES = [
 def extract_floor(floor_info: str) -> int:
     """층수 컬럼에서 실제 층수만 추출합니다.
 
-    현재 층수 정보는 'X out of Y'로 되어 있습니다.
+    현재 층수 정보는 'X out of Y'로 되어 있습니다. -> [X, out, of, Y]
     여기에서 X만 추출하여 정수로 반환합니다.
     Upper basement, Lower basement, Ground out 등은 모두 0층으로 변환합니다.
 
     Args:
         floor_info (str): 층수 정보
     """
-    # TODO
+    split_floor_info = floor_info.split()
+    floor_str = split_floor_info[0]
+
+    return int(floor_str) if floor_str.isnumeric() else 0
 
 
 def floor_extractor(df: pd.DataFrame, col: str) -> pd.DataFrame:
@@ -42,21 +45,23 @@ def floor_extractor(df: pd.DataFrame, col: str) -> pd.DataFrame:
     return df
 
 
-# TODO: 전처리 파이프라인 작성
+# 전처리 파이프라인 작성
 # 1. 방의 크기는 제곱근을 적용함 (FunctionTransformer 사용)
 # 2. 층수는 실제 층수를 추출하되 숫자가 아닌 Basement 등은 0층으로 표기함
 # 3. 범주형 변수(CAT_FEATURES)는 타겟 인코딩 적용 (from category_encoders import TargetEncoder)
 preprocess_pipeline = ColumnTransformer(
     transformers=[
-        # TODO,
+        ("sqrt_transformer", FunctionTransformer(np.sqrt), ["size"]),
         (
             "floor_extractor",
             FunctionTransformer(floor_extractor, kw_args={"col": "floor"}),
             ["floor"],
         ),
-        # TODO,
+        ("target_encoder", TargetEncoder(cols=CAT_FEATURES), CAT_FEATURES),
     ],
     remainder="passthrough",
     verbose_feature_names_out=False,
 )
+
+# dataframe 형태로 받기 위해 set_output 추가 필요(판다스 데이터 프레임 형태)
 preprocess_pipeline.set_output(transform="pandas")
